55336638c370579e5e03eab80036948dd9bb5a97
marrcelp <marcel.piaszczyk@gmail.com>
2024-07-26 15:31:14 +0200

    resolved exercise 2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+let z = 0;
+
+// for (let i = 0; i <= x; i++) {
+//     result += i;
+// }
+
+
+while (z <= 5) {
+    result += z;
+    z++;
+
+}
+
+console.log(result)
