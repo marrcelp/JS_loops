f0e188cce0ffeaf702c3ee61a9ee2a7625f3153b
marrcelp <marcel.piaszczyk@gmail.com>
2024-07-26 15:12:57 +0200

    resolved exercise 1

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log(`Wylosowana liczba to ${randomNumber}`);
+
+if (randomNumber > 5) {
+    for (let i = 5; i <= randomNumber; i++) {
+        console.log(i);
+    }
+} else {
+    console.log(`Wylosowana liczba jest zbyt mala zeby uzyc petli`)
+}
